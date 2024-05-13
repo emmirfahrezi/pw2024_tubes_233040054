29336746ec5ebfd89641cc9153261f7fa9c23fab
emmirfahrezi <emmir.fahrezi1@gmail.com>
2024-05-13 22:20:22 +0700

    nyicil

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<?php
+include 'partials/menu.php';
+?>
+
+
+<!-- navbar start -->
+
+
+<!-- navbar finish -->
+<section>
+    <h1>dashbor</h1>
+</section>
+
+
+<?php
+include 'partials/footer.php';
+?>
--- halaman/index.php
+++ halaman/index.php
@@ -6,35 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>monav classic</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="csshalaman/halaman.css">
+    <link rel="stylesheet" href="../css/halaman.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-transparent navbar-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">MONAV <span>CLASSIC</span></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto text-center ">
-                    <li class="nav-item">
+                <ul class="navbar-nav ms-auto text-center">
+                    <li class="nav-item mt-1">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <li class="nav-item mt-1">
+                        <a class="nav-link" href="#">Features</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <li class="nav-item mt-1">
+                        <a class="nav-link" href="#">Pricing</a>
                     </li>
-
-                    <button type="button" class="btn btn-success">Success</button>
-
+                    <li class="nav-item mt-1">
+                        <button type="button" class="btn btn-success me-1 custom-btn me-1 mb-1 ">Success</button>
+                    </li>
+                    <li class="nav-item mt-1">
+                        <button type="button" class="btn btn-warning me-1 custom-btn mb-3 ">Success</button>
+                    </li>
                 </ul>
             </div>
         </div>
     </nav>
-    <section class="halamansec">
+    <section id="halamansec">
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -43,24 +52,24 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="slide1.jpg" class="d-block w-100" alt="...">
+                    <img src="galery/slide1.jpg" class="d-block w-100" alt="...">
                     <div class="carousel-caption ">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <h5>MONAV CLASSIC</h5>
+                        <p>Kami menjuaal motor motor bekas yang siap di jual ke anda semua dengan harga murah dan terjangkau.</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="slide2.jpg" class="d-block w-100" alt="...">
+                    <img src="galery/slide2.jpg" class="d-block w-100" alt="...">
                     <div class="carousel-caption ">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
+                        <h5>MONAV CLASSIC</h5>
+                        <p>Kami juga menejual motor motor classic</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="slide3.jpg" class="d-block w-100" alt="...">
+                    <img src="galery/slide3.jpg" class="d-block w-100" alt="...">
                     <div class="carousel-caption">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
+                        <h5>MONAV CLASSIC</h5>
+                        <p>Kami menjual motor motor keren dan classic tapi modern</p>
                     </div>
                 </div>
             </div>
@@ -83,7 +92,7 @@
             <div class="row gy-4">
                 <div class="col-md-6">
                     <div class="card" ">
-                        <img src=" slide1.jpg" class="card-img-top" alt="...">
+                        <img src=" galery/bamc.png" class="card-img-top" alt="...">
                         <div class="card-body">
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
@@ -93,7 +102,7 @@
                     <div class="card mb-3" style="max-width: 540px;">
                         <div class="row g-0">
                             <div class="col-md-4">
-                                <img src="slide1.jpg" class="img-fluid rounded-start" alt="...">
+                                <img src="galery/bbmc.png" class="img-fluid rounded-start" alt="...">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
@@ -107,7 +116,7 @@
                     <div class="card mb-3" style="max-width: 540px;">
                         <div class="row g-0">
                             <div class="col-md-4">
-                                <img src="slide1.jpg" class="img-fluid rounded-start" alt="...">
+                                <img src="galery/hdci.png" class="img-fluid rounded-start" alt="...">
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
@@ -117,7 +126,6 @@
                             </div>
                         </div>
                     </div>
-
 
 
                 </div>
@@ -128,9 +136,54 @@
     </section>
 
     <section id="gallery">
-
-
+        <div class="containerg">
+            <div class="card2">
+                <img class="foto" src="slide1.jpg" alt="">
+            </div>
+            <div class="card2">
+                <img class="foto" src="slide1.jpg" alt="">
+            </div>
+            <div class="card2">
+                <img class="foto" src="slide1.jpg" alt="">
+            </div>
+            <div class="card2">
+                <img class="foto" src="slide1.jpg" alt="">
+            </div>
+            <div class="card2">
+                <img class="foto" src="slide1.jpg" alt="">
+            </div>
+        </div>
     </section>
+
+    <footer>
+        <div class="position-relative">
+            <div class="position-absolute top-0 start-0">
+
+            </div>
+            <div class="position-absolute top-0 start-50 translate-middle-x">halo</div>
+            <div class="position-absolute top-0 end-0">halo</div>
+            <div class="position-absolute top-50 start-0 translate-middle-y">hal;o</div>
+            <div class="position-absolute top-50 start-50 translate-middle">halo</div>
+            <div class="position-absolute top-50 end-0 translate-middle-y">halo</div>
+            <div class="position-absolute bottom-0 start-0">halo</div>
+            <div class="position-absolute bottom-0 start-50 translate-middle-x">halo</div>
+            <div class="position-absolute bottom-0 end-0">halo</div>
+        </div>
+    </footer>
+
+
+    <!-- javascript -->
+
+    <script>
+        const navEl = document.querySelector('.navbar');
+        window.addEventListener('scroll', () => {
+            if (window.scrollY >= 56) {
+                navEl.classList.add('navbar-scrolled');
+            } else {
+                navEl.classList.remove('navbar-scrolled');
+            }
+        });
+    </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
