312aa67960ac18f39a27b3e511f63818b04fb25b
emmirfahrezi <emmir.fahrezi1@gmail.com>
2024-05-28 18:01:14 +0700

    composer

--- admin/details.php
+++ admin/details.php
@@ -82,7 +82,7 @@ $mtr = query("SELECT * FROM motor WHERE id_motor=$id"); ?>
                                 <td><?= $mtr['harga'] ?></td>
                             </p>
                             <a href="ubah.php?id=<?= $mtr['id_motor']; ?>" class="btn btn-primary">EDIT</a>
-                            <a href="hapus.php?id=<?= $mtr['id_motor']; ?>" onclick="return confirm('yakin ingin mengapus foto?');" class="btn btn-danger">HAPUS</a>
+                            <a href="hapus.php?id=<?= $mtr['id_motor']; ?>" onclick="return confirm('yakin ingin mengapus data?');" class="btn btn-danger">HAPUS</a>
                         </div>
                         <a class="btn btn-warning" href="index.php" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
